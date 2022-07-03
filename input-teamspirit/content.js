9cb9e9f814f5d647f13bd33570516edc615c59ba
walktan <yb1.2st.50@gmail.com>
2022-07-03 21:33:36 +0900

    tmp

--- input-teamspirit/content.js
+++ input-teamspirit/content.js
@@ -1,11 +1,38 @@
+const startAt = "10:00";
+const endAt = "19:30";
+
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 	if (request == "Action") {
 		inputKintai();
 	}
 });
 
-function inputKintai() {
-  console.log("すたーと");
-  console.log($('iframe'));
-  console.log("えんど");
+const inputKintai = async () => {
+  console.log("start inputKintai()");
+  for ( const elem of $('table.month_frame td#mainTableArea tr td.vst.day_time0') ){
+    elem.click();
+    isClicked = true;;
+    await input(elem);
+    await update(elem);
+  };
+  console.log("end inputKintai()");
+}
+
+const input = (elem) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      $('#dlgInputTimeTabs #startTime').val(startAt);
+      $('#dlgInputTimeTabs #endTime').val(endAt);
+      $('button#dlgInpTimeOk').click();
+      resolve(true);
+    }, 1000)
+  })
+}
+
+const update = (elem) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(true);
+    }, 3000)
+  })
 }
